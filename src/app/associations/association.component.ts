3d9220badb4eceb67f80fd544e1b74b071c20fc0
feischml <metallaszlo@yahoo.com>
2017-06-26 22:01:22 +0300

    commit 26.06.2017

--- src/app/associations/association.component.ts
+++ src/app/associations/association.component.ts
@@ -1,18 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { AssociationService } from './service/association.service';
+import { Association } from './model/association.model';
 
 @Component({
-    templateUrl: 'association.template.html'
+    templateUrl: 'association.template.html',
+    providers: [AssociationService]
 })
 export class AssociationComponent implements OnInit{
 
     componentTitle = "Associations";
 
-    constructor(){
-        
-    }
+    // List of Associations
+    associations = [];
+
+    constructor(private _associationsService: AssociationService){ }
 
     ngOnInit(){
+        // Load the Associations list
+        this._associationsService.getAssociations().subscribe(
+            associations => {
+                this.associations = associations
+            },
+            err => {
+                console.log(err);
+        });
+    }
 
+    deleteAssociation(association: Association){
+        this._associationsService.deleteAssociation(association).subscribe(
+            associations => {
+                let index = this.associations.indexOf(association);
+                this.associations.splice(index,1);
+            },
+            err => {
+                console.log(err);
+        });
     }
 
 }
--- src/app/associations/service/association.service.ts
+++ src/app/associations/service/association.service.ts
@@ -1,11 +1,69 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { AppConstants} from '../../app.constants';
+import { Association} from '../model/association.model';
 
 @Injectable()
 export class AssociationService{
 
-    constructor(){
+    // Application Constants
+    private appConstants;
 
+    constructor( private _http: Http ){
+        this.appConstants = new AppConstants();
+     }
+
+     // Return the list of Associations from the server
+    getAssociations(){
+        var route = '/associations/associations';
+        return this._http.get(
+                this.appConstants.getServerUrl() + route,
+                { headers: this.appConstants.getHeaders() }).
+            map(roles => roles.json());
+    }
+
+    // Return an Association with a given id
+    getAssociation(id: String){
+        var route = '/associations/associationid/' + id;
+        return this._http.get(
+                this.appConstants.getServerUrl() + route,
+                { headers: this.appConstants.getHeaders() }).
+            map( role => role.json());        
+    }
+
+    // Update or create an Association
+    updateCreateAssociation(association: Association){
+        var route;
+        if (!association._id){
+            route = '/associations/create';
+            return this._http.post(
+                    this.appConstants.getServerUrl() + route, 
+                    JSON.stringify(association),
+                    { headers: this.appConstants.getHeaders() }
+                ).map(res => res.json());
+        } else {
+            route = '/associations/update';  
+            return this._http.put(
+                    this.appConstants.getServerUrl() + route, 
+                    JSON.stringify(association),
+                    { headers: this.appConstants.getHeaders() }
+                ).map(res => res.json());
+        }
+    }
+
+    // Delete Association
+    deleteAssociation(association: Association){
+        var route = '/assotiations/';
+        if (association._id){
+            route = route + association._id;
+            return this._http.delete(
+                this.appConstants.getServerUrl() + route,
+                { headers: this.appConstants.getHeaders() }
+            ).map( res => res.json());
+        } else {
+            alert("Can't delete Association, inconsistent data.");
+        }
     }
 
 }
