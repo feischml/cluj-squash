97b5749485439c897de032449da7e8634e6cd0d1
feischml <metallaszlo@yahoo.com>
2017-07-06 22:42:16 +0300

    commit 06.07.2017

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventsService } from './service/events.service';
+import { User } from '../people/model/users.model';
+import { LclStorageService } from '../lclstorage/lclstorage.service';
+import { AppConstants } from '../app.constants';
 
 @Component({
     templateUrl: 'events.template.html',
     styleUrls: ['styles/events.style.css'],
     providers: [ EventsService ]
 })
-export class EventsComponent{
+export class EventsComponent implements OnInit{
 
     componentTitle = "Events";
     // Listr of events
     events = [];
 
+    // Get logged in User
+    loggedUser: User;
+    private lclStorage: LclStorageService;
+
     constructor(private _eventsservice: EventsService){ 
+        // Get logged user info
+        this.lclStorage = new LclStorageService();
+        this.loggedUser = JSON.parse(this.lclStorage.getItem(AppConstants.LOGGED_USER));
+
         this._eventsservice.getEvents().subscribe(
             events => { 
                 this.events = events
@@ -21,6 +32,10 @@ export class EventsComponent{
                 console.log(error);
             }
         )
+    }
+
+    ngOnInit(){
+        // Todo when loggen in/out refresh Register button
     }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { User } from '../people/model/users.model';
+import { LclStorageService } from '../lclstorage/lclstorage.service';
+import { AppConstants } from '../app.constants';
 
 @Component({
     templateUrl: './home.template.html'
 })
 export class HomeComponent{
+
+    // Get logged in User
+    loggedUser: User;
+    private lclStorage: LclStorageService;
+
+    constructor(){
+        this.lclStorage = new LclStorageService();
+        this.loggedUser = JSON.parse(this.lclStorage.getItem(AppConstants.LOGGED_USER));
+    }
 
 }
